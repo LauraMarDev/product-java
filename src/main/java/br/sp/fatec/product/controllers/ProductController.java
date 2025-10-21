2caddebdb93df5c78a469ee62776444ffed311e9
Laura Marques <lauraosmor@gmail.com>
2025-10-21 11:19:26 -0300

    third commit

--- src/main/java/br/sp/fatec/product/controllers/ProductController.java
+++ src/main/java/br/sp/fatec/product/controllers/ProductController.java
@@ -1,33 +1,24 @@
 package br.sp.fatec.product.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.sp.fatec.product.entities.Product;
-import jakarta.persistence.EntityNotFoundException;
+import br.sp.fatec.product.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("products")
 public class ProductController { //serve como endpoint
-    
-    private ArrayList<Product> list = new ArrayList<Product>();
 
-    public ProductController(){ //método construtor, nome igual à classe
-        list.add(new Product(1L, "product 1", "description1", 100.0));
-        list.add(new Product(2L, "product 2", "description2", 200.0));
-        list.add(new Product(3L, "product 3", "description3", 300.0));
-    }
+    @Autowired
+    private ProductRepository repository;
 
-    @GetMapping("{id}") //chama o mapeamento do products e exibe a mensagem dentro da string
-    public Product getProductsById(@PathVariable long id) {
-        return list.stream()
-                    .filter(p -> p.getId() == id)
-                    .findFirst()
-                    .orElseThrow( () -> new EntityNotFoundException("Não cadastrado"));
+    @GetMapping
+    public List<Product> getProducts(){
+        return repository.findAll();
     }
 }
